abd3dff16a5ffb3b632a12bebfba098a7cd2c239
unleashed <kkipkurui123@gmail.com>
2023-03-11 08:34:20 +0300

    add file

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,36 @@
+const { library } = require('@fortawesome/fontawesome-svg-core');
+const { fas } = require('@fortawesome/free-solid-svg-icons');
+
+library.add(fas);
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean:true,
-    },
-  plugins:[
-    new  HtmlWebpackPlugin({
-        title: 'My Webpack Project',
-        template: './src/index.html'
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'My Webpack Project',
+      template: './src/index.html',
     }),
   ],
   module: {
     rules: [
-        {
+      {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-        },
+      },
     ],
   },
   devServer: {
     static: {
-        directory: path.join(__dirname, 'dist'),
+      directory: path.join(__dirname, 'dist'),
     },
     watchFiles: ['./'],
     port: 8080,

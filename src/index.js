ed3091a7b95d655cc3286fd4829485452630682d
unleashed <kkipkurui123@gmail.com>
2023-03-11 08:30:41 +0300

    add index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css';
+import TaskList from './modules/list.js';
+
+const tasklist = new TaskList();
+
+window.onload = () => {
+  tasklist.displaylist();
+};
+
+const form = document.getElementById('form-input');
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const input = event.target.querySelector('input');
+  const description = input.value.trim();
+
+  if (description) {
+    tasklist.addTask(description);
+    input.value = '';
+  }
+});
+
+const clearButton = document.getElementById('clear-button');
+clearButton.addEventListener('click', () => {
+  tasklist.removeTasks();
+});

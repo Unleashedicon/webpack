20f52255f87fc8e8243c9c1401366354033af358
unleashed <kkipkurui123@gmail.com>
2023-03-12 00:56:09 +0300

    update file

--- src/modules/load.js
+++ src/modules/load.js
@@ -1,19 +1,2 @@
-const tasks = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Task 2',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 2,
-  },
-];
 const load = () => JSON.parse(localStorage.getItem('tasks')) || tasks;
 export default load;
